10c7fa8439d273f0935ab61eaeff887715a1a935
valdircaf <valdircaf@hotmail.com>
2023-11-16 21:21:37 -0300

    first commit

---
+++ src/main/java/com/cadastro/cadastro/CadastroApplication.java
@@ -0,0 +1,13 @@
+package com.cadastro.cadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroApplication.class, args);
+	}
+
+}
